1f892220e7cbfc65e11929d90d35f89a82117993
Eric Di Re <eric.di.re@gmail.com>
2023-06-09 06:24:02 -0700

    updated missing funnel test for new string column

--- tests/assert_no_missing_funnels.sql
+++ tests/assert_no_missing_funnels.sql
@@ -8,7 +8,7 @@
 select distinct a.funnel_id
 from {{ ref('fct_optins_activity') }} a
     left join `bbg-platform.analytics.dim_funnels` f
-        on cast(a.funnel_id as int) = f.funnel_id
+        on a.funnel_id = f.funnel_id
 where f.funnel_id is null
     and a.funnel_id is not null
 
@@ -17,6 +17,6 @@ union distinct
 select distinct d.funnel_id
 from {{ ref('stg_kbb_evergreen__orders') }} d
     left join `bbg-platform.analytics.dim_funnels` f
-        on cast(d.funnel_id as int) = f.funnel_id
+        on d.funnel_id = f.funnel_id
 where f.funnel_id is null
     and d.funnel_id is not null
